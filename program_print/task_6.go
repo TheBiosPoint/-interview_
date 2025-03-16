517e821923af201916049179ee011404565fc4ba
TheBiosPoint <145383494+TheBiosPoint@users.noreply.github.com>
2025-03-16 21:03:42 +0300

    Update task_6.go

--- program_print/task_6.go
+++ program_print/task_6.go
@@ -1,1 +1,14 @@
+package main
 
+import "fmt"
+
+func main() {
+	a := make([]int, 0)
+	a = append(a, 1)
+	a = append(a, 2)
+	a = append(a, 3)
+	b := append(a, 4)
+	c := append(a, 5)
+
+	fmt.Printf("%v %v %v", a , b , c)
+}
